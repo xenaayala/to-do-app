4fbf01ebffcc28ed13195bb1463fc3d58f40e2ef
xena <xenaayala96@gmail.com>
2020-03-06 11:26:06 -0500

    Updates

---
+++ precache-manifest.e3743501aff2b2046f3b52a5ae5e09bb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a190c307473487172caf4603cda6d2fa",
+    "url": "/to-do-app/index.html"
+  },
+  {
+    "revision": "09ad2ddbbe22b8e95b8c",
+    "url": "/to-do-app/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "7a4474838245f964156d",
+    "url": "/to-do-app/static/css/main.7999f849.chunk.css"
+  },
+  {
+    "revision": "09ad2ddbbe22b8e95b8c",
+    "url": "/to-do-app/static/js/2.ce932725.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/to-do-app/static/js/2.ce932725.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a4474838245f964156d",
+    "url": "/to-do-app/static/js/main.41e14a86.chunk.js"
+  },
+  {
+    "revision": "72667002db99cce95a37",
+    "url": "/to-do-app/static/js/runtime-main.00d98190.js"
+  },
+  {
+    "revision": "87f56b3d73a95c4133222b7dd4377c65",
+    "url": "/to-do-app/static/media/background.87f56b3d.jpg"
+  }
+]);
